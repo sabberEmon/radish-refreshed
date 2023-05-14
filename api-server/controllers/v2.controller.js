7b383e1b7b433c59fd0fa20517afd55ebf8b8271
sabberEmon <sabberemon@outlook.com>
2023-05-15 00:37:16 +0600

    v2 api

--- api-server/controllers/v2.controller.js
+++ api-server/controllers/v2.controller.js
@@ -35,3 +35,44 @@ exports.getNftById = async (req, res) => {
     });
   }
 };
+
+exports.getNftByQuery = async (req, res) => {
+  const { collectionIdentifier, referenceId } = req.query;
+
+  if (!collectionIdentifier || !referenceId) {
+    return res.status(400).json({
+      success: false,
+      error: "missing_parameters",
+      message: "Missing parameters collectionIdentifier or referenceId or both",
+    });
+  }
+
+  try {
+    const nft = await Nft.findOne({
+      collectionIdentifier,
+      referenceId,
+    });
+
+    if (!nft) {
+      return res.status(400).json({
+        success: false,
+        error: "nft_not_found",
+        message: "Nft not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      error: null,
+      message: "Nft fetched successfully",
+      nft,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "server_error",
+      message: "Internal server error",
+    });
+  }
+};
--- api-server/routes/v2.route.js
+++ api-server/routes/v2.route.js
@@ -4,6 +4,7 @@ const v2Controller = require("../controllers/v2.controller");
 const router = express.Router();
 
 // routes
+router.get("/nft", v2Controller.getNftByQuery);
 router.get("/nfts/:id", v2Controller.getNftById);
 
 module.exports = router;
